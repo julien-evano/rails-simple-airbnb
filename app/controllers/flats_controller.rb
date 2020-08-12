9f0bc17e9ebaca3e4699ae7045cd073f0608e60f
julien evano <julien.evano@audencia.com>
2020-08-12 18:08:29 +0200

    salut

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,51 @@
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to @flat, notice: 'Flat was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.new
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: 'Flat successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, notice: 'Restaurant successfully destroyed'
+  end
+
+  def search
+    @flats = Flat.where("name LIKE '%house%'")
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+
+flat1 = { name: 'Beautiful house', address: 'Canada', description: 'cosy', price_per_night: 100, number_of_guests: 5 }
+flat2 = { name: 'Cosy appart', address: 'Paris', description: 'cheap', price_per_night: 1000, number_of_guests: 3 }
+flat3 = { name: 'Tiny house', address: 'New York', description: 'nice view', price_per_night: 250, number_of_guests: 4 }
+flat4 = { name: 'Random flat', address: 'Los Angeles', description: 'funny', price_per_night: 50, number_of_guests: 1 }
+
+[flat1, flat2, flat3, flat4].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "valide ? #{flat.valid?}"
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
